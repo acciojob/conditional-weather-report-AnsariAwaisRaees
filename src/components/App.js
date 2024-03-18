f5cf9a36ab1ad8637e1e7e3babf68a91d31bf545
DELL <ansariawaisraees@gamil.com>
2024-03-18 15:00:11 +0530

    Solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weatherInput, setWeatherInput] = useState({tempreture: 25, conditions: "Sunny"});
+  const [textColor, setTextColor] = useState("blue");
+
+  useEffect(() => {
+    if (weatherInput.tempreture > 20) {
+      setTextColor("red");
+    }
+    else{
+      setTextColor("blue");
+    }
+  }, [weatherInput.tempreture]);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div style = {{color: textColor}}>
+          <p>Tempreture: {weatherInput.tempreture}</p>
+          <p>conditions: {weatherInput.conditions}</p>
+        </div>
     </div>
   )
 }
