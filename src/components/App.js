b5b7688b4662b05a977ba3faa870de4fcc8e4022
DELL <ansariawaisraees@gamil.com>
2024-03-18 15:21:01 +0530

    Solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './../styles/App.css';
-import WeatherApp from './WeatherApp';
 
 const App = () => {
+  const weather = { temperature:25, conditions: "Sunny"};
+  const temperatureColor = weather.temperature > 20 ? 'red' : 'blue';
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-        <WeatherApp/>
+    <div id="main">
+        <h2>Weather Report</h2>
+        <p>Temperature: <span style={{color:temperatureColor}}>{weather.temperature}</span></p>
+        <p>Conditions: {weather.conditions}</p>
     </div>
   )
 }
